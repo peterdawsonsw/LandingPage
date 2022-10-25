83cd47b6512dd54e4d8d6d8e2233c29a1028880f
= <=>
2022-10-25 17:33:49 -0400

    changed name

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: "My App",
+  siteName: "LandingPage",
   siteDescription: "A Gridsome starter with Chakra-UI",
   plugins: [],
   icon: "./src/assets/images/favicon.png",
